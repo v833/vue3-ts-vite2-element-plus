59c535809efbcdfe139724785a9a8c18d1e82b01
v833 <2507301541@qq.com>
2022-03-06 22:01:05 +0800

    feat: 安装jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
 const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VueJsx()],
   server: {
     port: 8080,
   },
